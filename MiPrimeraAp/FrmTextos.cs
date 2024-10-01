5b3d67e5498a11a50916784b552ce6e9c11021cc
RodriGitTS <trsrodri@gmail.com>
2024-10-01 10:22:30 +0200

    Boton act/desac nombre

--- MiPrimeraAp/FrmTextos.cs
+++ MiPrimeraAp/FrmTextos.cs
@@ -37,7 +37,15 @@ namespace MiPrimeraAp
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            if (button3.Text == "Desactivar")
+            {
+                txtApellido.Enabled = true;
+            }
+            else
+            {
+                button3.Text= "Activar";
+                txtApellido.Enabled=false;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
